391465d070111a4701f8016c95392ff3b36126fe
tsshn <ilya.yatsyshyn@gmail.com>
2020-06-11 23:00:42 +0300

    gitignore updated

--- Customer.py
+++ Customer.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 class User(object):
 
     def __init__(self, name, surname, userid):
@@ -13,12 +14,11 @@ class User(object):
                                 password='2a58a610fa0064b79d45735606512a9994a9c43a4b2ba30dc3467ae3c375af26',
                                 host='ec2-18-232-143-90.compute-1.amazonaws.com')
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO users  VALUES(%s, %s, %s)", (self.userid,self.name, self.surname ))
+        cursor.execute("INSERT INTO users  VALUES(%s, %s, %s)", (self.userid, self.name, self.surname))
         conn.commit()
         cursor.close()
         conn.close()
         return "Braking"
 
     def check(self):
-
         return "I'm driving!"
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import psycopg2
 import Customer
 from telebot import types
 
-autorization = 0
+authorization = 0
 name = ''
 surname = ''
 
@@ -26,15 +26,20 @@ keyboard1.row('Привіт', 'Бувай')
 keyboard1.row('Де я?')
 keyboard1.row('/autorization', '/users', '/geophone')
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Доброго дня, козаче! Я бот що здає в аренду електросамокати та велосипеди. Якщо хочешь щось від мене, то натискай на кнопки))0)0)', reply_markup=keyboard1)
+    bot.send_message(message.chat.id,
+                     'Доброго дня, козаче! Я бот що здає в аренду електросамокати та велосипеди. Якщо хочешь щось від мене, то натискай на кнопки))0)0)',
+                     reply_markup=keyboard1)
+
 
 @bot.message_handler(commands=['autorization'])
 def autorization(message):
     bot.send_message(message.chat.id, 'Авторизація. Напиши мені своє ім\'я', reply_markup=keyboard1)
-    global autorization
-    autorization = 1
+    global authorization
+    authorization = 1
+
 
 @bot.message_handler(commands=['users'])
 def users(message):
@@ -46,10 +51,13 @@ def users(message):
     records = cursor.fetchall()
 
     for i in range(len(records)):
-        bot.send_message(message.chat.id, '%s користувач: %s, %s, %s' % (i+1,records[i][1],records[i][2],records[i][0]), reply_markup=keyboard1)
+        bot.send_message(message.chat.id,
+                         '%s користувач: %s, %s, %s' % (i + 1, records[i][1], records[i][2], records[i][0]),
+                         reply_markup=keyboard1)
 
     cursor.close()
     conn.close()
+
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -63,25 +71,26 @@ def geophone(message):
     button_phone = types.KeyboardButton(text="Відправити номер телефона", request_contact=True)
     button_geo = types.KeyboardButton(text="Відправити геолокацію", request_location=True)
     keyboard.add(button_phone, button_geo)
-    bot.send_message(message.chat.id, "Ти можеш відправити мені свій номер телефона або геолокацію!", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Ти можеш відправити мені свій номер телефона або геолокацію!",
+                     reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    global autorization
+    global authorization
 
-    if(autorization == 1):
-        bot.send_message(message.chat.id,'Добре , тепер напиши мені своє прізвище) ')
+    if (authorization == 1):
+        bot.send_message(message.chat.id, 'Добре , тепер напиши мені своє прізвище) ')
         global name
         name = message.text
-        autorization = 2
-    elif(autorization == 2):
+        authorization = 2
+    elif (authorization == 2):
         bot.send_message(message.chat.id, 'Тебе успішно авторизовано!) ')
         global surname
         surname = message.text
-        user = Customer.User(name,surname,message.from_user.id)
+        user = Customer.User(name, surname, message.from_user.id)
         user.add()
-        autorization = 0
+        authorization = 0
     else:
         if message.text.lower() == 'привіт':
             bot.send_message(message.chat.id, 'Категорично вас вітаю пане, ' + message.from_user.first_name)
@@ -89,10 +98,12 @@ def send_text(message):
             bot.send_message(message.chat.id, 'Допобачення')
         elif message.text.lower() == 'де я?':
             bot.send_message(message.chat.id, 'Ось ти де')
-            bot.send_location(message.chat.id, 49.33273504 , 17.61087799)
+            bot.send_location(message.chat.id, 49.33273504, 17.61087799)
+
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
     print(message)
+
 
 bot.polling(none_stop=True, interval=0)
