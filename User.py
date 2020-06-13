6a7c19e3fd7aec1259873b5e0339e2c670e34031
tsshn <ilya.yatsyshyn@gmail.com>
2020-06-13 18:40:08 +0300

    registration partly implemented

--- User.py
+++ User.py
@@ -1,11 +1,11 @@
 class User(object):
 
-    def __init__(self, identifier, name, surname, number, birth_date, country, city, bonus_points):
-        self.identifier = identifier
-        self.name = name
-        self.surname = surname
-        self.number = number
-        self.birth_date = birth_date
-        self.country = country
-        self.city = city
-        self.bonus_points = bonus_points
+    def __init__(self):
+        self.identifier = None
+        self.name = None
+        self.surname = None
+        self.number = None
+        self.birth_date = None
+        self.country = None
+        self.city = None
+        self.bonus_points = None
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import User
 import Employee
 from telebot import types
 from datetime import datetime
+from pprint import pprint
 
 from Controller import Controller
 
@@ -14,50 +15,42 @@ controller = Controller("d6ib69jeupvh36", "szvriplnadxleq",
 bot = telebot.TeleBot('1198725614:AAECjKvTD7fpK_rO21vxsBpNYwKgJJluxC8')
 
 state = ""
+state = 0
+current_user = User.User()
 
-
-# @bot.message_handler(content_types=['text'])
-# def send_text(message):
-#     global state
-#     user = User.User(message.from_user.id)
-#
-#     if state == "name":
-#         bot.send_message(message.chat.id, 'Enter your name')
-#         user.name = message.text
-#         state = "surname"
-#     elif state == "surname":
-#         bot.send_message(message.chat.id, 'Enter your name')
-#         user.name = message.text
-#         state = "number"
-#         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-#         number_button = types.KeyboardButton(text="Send number", request_contact=True)
-#         keyboard.add(number_button)
-#
-# @bot.message_handler(content_types=['contact'])
-# def report(message):
-#     print(message)
-
-# else:
-#     if message.text.lower() == 'привіт':
-#         bot.send_message(message.chat.id, 'Категорично вас вітаю пане, ' + message.from_user.first_name)
-#     elif message.text.lower() == 'бувай':
-#         bot.send_message(message.chat.id, 'Допобачення')
-#     elif message.text.lower() == 'де я?':
-#         bot.send_message(message.chat.id, 'Ось ти де')
-#         bot.send_location(message.chat.id, 49.33273504, 17.61087799)
-
-
-@bot.callback_query_handler(lambda query: query.data == "no_register")
-def register2(query):
-    @bot.message_handler(content_types=['text'])
-    def send_text(message):
-        print(message.text + " - No")
 
 @bot.callback_query_handler(lambda query: query.data == "register")
-def register1(query):
-    @bot.message_handler(content_types=['text'])
-    def send_text(message):
-        print(message.text + " - Yes")
+def register(query):
+    global state
+    state = 1
+    current_user.identifier = query.message.from_user.id
+    bot.send_message(query.message.chat.id, "Enter your name")
+
+
+@bot.message_handler(func=lambda message: state == 1)
+def user_name(message):
+    global state
+    state = 2
+    current_user.name = message.text
+    bot.send_message(message.chat.id, "Enter your surname")
+
+
+@bot.message_handler(func=lambda message: state == 2)
+def user_surname(message):
+    global state
+    state = 3
+    current_user.surname = message.text
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    send_button = types.KeyboardButton(text="Send", request_contact=True)
+    keyboard.add(send_button)
+    bot.send_message(message.chat.id, "Send your phone number", reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=['contact'])
+def get_contact(message):
+    bot.send_message(message.chat.id, 'О класс спасибо за контакт')
+    print(message.contact.phone_number)
+    current_user.number = message.contact.phone_number
 
 
 @bot.message_handler(commands=['start'])
@@ -66,114 +59,13 @@ def start_message(message):
     if len(users) == 0:
         keyboard = types.InlineKeyboardMarkup()
         yes_button = types.InlineKeyboardButton(text="Yes", callback_data="register")
-        no_button = types.InlineKeyboardButton(text="No", callback_data="no_register")
-        keyboard.add(yes_button, no_button)
-
+        keyboard.add(yes_button)
         bot.send_message(message.chat.id,
                          'Hello, it seems you are not registered.\nDo you want to register?',
                          reply_markup=keyboard)
-
     else:
         bot.send_message(message.chat.id,
                          'Hello, ' + users[0][1])
 
 
 bot.polling(none_stop=True, interval=0)
-
-# controller.close()
-
-# authorization = 0
-# name = ''
-# surname = ''
-#
-# conn = psycopg2.connect(dbname='d6ib69jeupvh36', user='szvriplnadxleq',
-#                         password='3f6a5c41af6e1ea4a4cc136566588d23fc243823e23a4c2498de18c01865ac3a',
-#                         host='ec2-34-197-188-147.compute-1.amazonaws.com')
-# cursor = conn.cursor()
-# cursor.execute('SELECT * FROM users')
-# records = cursor.fetchall()
-# print(records[1][1])
-# for i in range(len(records)):
-#     print(records[i][1])
-# print(records)
-# cursor.close()
-# conn.close()
-#
-# bot = telebot.TeleBot('1198725614:AAECjKvTD7fpK_rO21vxsBpNYwKgJJluxC8')
-# keyboard1 = telebot.types.ReplyKeyboardMarkup()
-# keyboard1.row('Привіт', 'Бувай')
-# keyboard1.row('Де я?')
-# keyboard1.row('/authorization', '/users', '/geo_position')
-#
-#
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     bot.send_message(message.chat.id,
-#                      'Hi, it is a really beginning of our messaging',
-#                      reply_markup=keyboard1)
-#
-#
-# @bot.message_handler(commands=['authorization'])
-# def authorization(message):
-#     bot.send_message(message.chat.id, 'Авторизація. Напиши мені своє ім\'я', reply_markup=keyboard1)
-#     global authorization
-#     authorization = 1
-#
-#
-# @bot.message_handler(commands=['users'])
-# def users(message):
-#     cursor.execute('SELECT * FROM users')
-#     records = cursor.fetchall()
-#
-#     for i in range(len(records)):
-#         bot.send_message(message.chat.id,
-#                          '%s користувач: %s, %s, %s' % (i + 1, records[i][1], records[i][2], records[i][0]),
-#                          reply_markup=keyboard1)
-#
-# @bot.message_handler(commands=['help'])
-# def help(message):
-#     bot.send_message(message.chat.id, 'Пиши /start і починай роботу!', reply_markup=keyboard1)
-#
-#
-# @bot.message_handler(commands=["geo_position"])
-# def geo_position(message):
-#     # Эти параметры для клавиатуры необязательны, просто для удобства
-#     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-#     button_phone = types.KeyboardButton(text="Відправити номер телефона", request_contact=True)
-#     button_geo = types.KeyboardButton(text="Відправити геолокацію", request_location=True)
-#     keyboard.add(button_phone, button_geo)
-#     bot.send_message(message.chat.id, "Ти можеш відправити мені свій номер телефона або геолокацію!",
-#                      reply_markup=keyboard)
-#
-#
-# @bot.message_handler(content_types=['text'])
-# def send_text(message):
-#     global authorization
-#
-#     if authorization == 1:
-#         bot.send_message(message.chat.id, 'Добре , тепер напиши мені своє прізвище) ')
-#         global name
-#         name = message.text
-#         authorization = 2
-#     elif authorization == 2:
-#         bot.send_message(message.chat.id, 'Тебе успішно авторизовано!) ')
-#         global surname
-#         surname = message.text
-#         user = User.User(name, surname, message.from_user.id)
-#         user.add()
-#         authorization = 0
-#     else:
-#         if message.text.lower() == 'привіт':
-#             bot.send_message(message.chat.id, 'Категорично вас вітаю пане, ' + message.from_user.first_name)
-#         elif message.text.lower() == 'бувай':
-#             bot.send_message(message.chat.id, 'Допобачення')
-#         elif message.text.lower() == 'де я?':
-#             bot.send_message(message.chat.id, 'Ось ти де')
-#             bot.send_location(message.chat.id, 49.33273504, 17.61087799)
-#
-#
-# @bot.message_handler(content_types=['sticker'])
-# def sticker_id(message):
-#     print(message)
-#
-# bot.polling(none_stop=True, interval=0)
