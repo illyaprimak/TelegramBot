a0e109ae3d0442ef8a6993c6ab3b4eac5a84bece
tsshn <ilya.yatsyshyn@gmail.com>
2020-06-13 21:35:13 +0300

    base authorization implemented

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import telebot
+import datetime
+import geopy
+import telebot_calendar
 import psycopg2
 import User
 import Employee
 from telebot import types
-from datetime import datetime
 from pprint import pprint
-
+from telebot_calendar import CallbackData
+from telebot.types import ReplyKeyboardRemove, CallbackQuery
+from geopy.geocoders import Nominatim
+from datetime import datetime
 from Controller import Controller
 
 controller = Controller("d6ib69jeupvh36", "szvriplnadxleq",
@@ -14,58 +19,122 @@ controller = Controller("d6ib69jeupvh36", "szvriplnadxleq",
 
 bot = telebot.TeleBot('1198725614:AAECjKvTD7fpK_rO21vxsBpNYwKgJJluxC8')
 
-state = ""
-state = 0
 current_user = User.User()
-
-
-@bot.callback_query_handler(lambda query: query.data == "register")
-def register(query):
-    global state
-    state = 1
-    current_user.identifier = query.message.from_user.id
-    bot.send_message(query.message.chat.id, "Enter your name")
-
-
-@bot.message_handler(func=lambda message: state == 1)
-def user_name(message):
-    global state
-    state = 2
-    current_user.name = message.text
-    bot.send_message(message.chat.id, "Enter your surname")
-
-
-@bot.message_handler(func=lambda message: state == 2)
-def user_surname(message):
-    global state
-    state = 3
-    current_user.surname = message.text
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    send_button = types.KeyboardButton(text="Send", request_contact=True)
-    keyboard.add(send_button)
-    bot.send_message(message.chat.id, "Send your phone number", reply_markup=keyboard)
-
-
-@bot.message_handler(content_types=['contact'])
-def get_contact(message):
-    bot.send_message(message.chat.id, 'О класс спасибо за контакт')
-    print(message.contact.phone_number)
-    current_user.number = message.contact.phone_number
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     users = controller.user_exists(message.from_user.id)
     if len(users) == 0:
-        keyboard = types.InlineKeyboardMarkup()
-        yes_button = types.InlineKeyboardButton(text="Yes", callback_data="register")
-        keyboard.add(yes_button)
+        keyboard = types.ReplyKeyboardMarkup()
+        keyboard.row('/register')
         bot.send_message(message.chat.id,
                          'Hello, it seems you are not registered.\nDo you want to register?',
                          reply_markup=keyboard)
     else:
         bot.send_message(message.chat.id,
                          'Hello, ' + users[0][1])
+
+
+@bot.message_handler(commands=['register'])
+def register(message):
+    current_user.identifier = message.from_user.id
+    current_user.bonus_points = 0
+
+    message = bot.send_message(message.chat.id, "Enter your name", reply_markup=None)
+
+    bot.register_next_step_handler(message, user_name)
+
+
+@bot.message_handler(content_types=['text'])
+def default(message):
+    bot.send_message(message.chat.id, "Unknown command")
+
+
+def user_name(message):
+    current_user.name = message.text
+
+    message = bot.send_message(message.chat.id, "Enter your surname")
+
+    bot.register_next_step_handler(message, user_surname)
+
+
+def user_surname(message):
+    current_user.surname = message.text
+
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    send_button = types.KeyboardButton(text="Send", request_contact=True)
+    keyboard.add(send_button)
+
+    message = bot.send_message(message.chat.id, "Send your phone number", reply_markup=keyboard)
+
+    bot.register_next_step_handler(message, user_contact)
+
+
+@bot.message_handler(content_types=['contact'])
+def user_contact(message):
+    current_user.number = message.contact.phone_number
+
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    send_button = types.KeyboardButton(text="Send", request_location=True)
+    keyboard.add(send_button)
+    message = bot.send_message(message.chat.id, 'Send your location',
+                               reply_markup=keyboard)
+
+    bot.register_next_step_handler(message, user_location)
+
+
+@bot.message_handler(content_types=['location'])
+def user_location(message):
+    locator = Nominatim(user_agent="myGeocoder")
+    location = locator.reverse(str(message.location.latitude) + ", " + str(message.location.longitude))
+    current_user.country = location.raw['address']['country']
+    current_user.city = location.raw['address']['city']
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton("Choose", callback_data="getdate")
+    )
+    bot.send_message(
+        message.chat.id, "Choose your birthday date", reply_markup=keyboard
+    )
+
+
+calendar = CallbackData("calendar_1", "action", "year", "month", "day")
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith(calendar.prefix))
+def callback_inline(call: CallbackQuery):
+    name, action, year, month, day = call.data.split(calendar.sep)
+    date = telebot_calendar.calendar_query_handler(
+        bot=bot, call=call, name=name, action=action, year=year, month=month, day=day
+    )
+    if action == "DAY":
+        current_user.birth_date = date
+    elif action == "CANCEL":
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.row(
+            telebot.types.InlineKeyboardButton("Choose", callback_data="getdate")
+        )
+        bot.send_message(
+            call.from_user.id, "Please choose your birthday date", reply_markup=keyboard
+        )
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("getdate"))
+def callback_inline(call: CallbackQuery):
+    if call.data == "getdate":
+        now = datetime.now()
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text="Selected date",
+            reply_markup=telebot_calendar.create_calendar(
+                name=calendar.prefix,
+                year=now.year,
+                month=now.month,
+            ),
+        )
 
 
 bot.polling(none_stop=True, interval=0)
